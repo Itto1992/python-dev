55b9203239a9c910f01fbbfffbe96a16d57915d3
Itto <itto.higuchi@gmail.com>
2022-08-12 21:20:38 +0900

    Add vim-sonictemplate and a template for atcoder

---
+++ .config/nvim/templates/python/base-atcoder.py
@@ -0,0 +1,20 @@
+import bisect
+import itertools
+import math
+import sys
+from collections import Counter
+
+input = sys.stdin.readline
+
+
+def main():
+    N = int(input())
+    A = map(int, input().split())
+
+    ret = 0
+
+    print(ret)
+
+
+if __name__ == '__main__':
+    main()
